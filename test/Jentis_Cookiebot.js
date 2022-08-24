9601fec0954f3fbbb663097447b230f87fc84f1b
Daniel Dorn <donny@front-design.at>
2022-08-24 14:33:20 +0200

    Hotfix: "lastupdate"

--- test/Jentis_Cookiebot.js
+++ test/Jentis_Cookiebot.js
@@ -21,7 +21,8 @@ function updateConsentData(cookiebot)
 {
 	var consentDataLayer = {};
 	consentDataLayer.consentid = window.jentis.consent.engine.getConsentId();
-	consentDataLayer.lastupdate = window.jentis.consent.engine.getLastUpdateTime();
+	consentDataLayer.lastupdate = Date.now();
+	//consentDataLayer.lastupdate = window.jentis.consent.engine.getLastUpdateTime();
 
 	var vendorData = window.jentis.consent.engine.getVendorFullData();
 	for (const key in vendorData) 
@@ -44,5 +45,5 @@ function updateConsentData(cookiebot)
 	
 	console.log(consentDataLayer);
 	jentis.consent.engine.setNewVendorConsents(consentDataLayer);
-
+	
 }
